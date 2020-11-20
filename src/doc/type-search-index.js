578bf92d091f6ad4f5126c9ee64e4e5c57a1fc51
Chotiwit <nut.chotiwit@gmail.com>
2020-11-21 01:05:36 +0700

    Add java doc to Whole Project

---
+++ src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.company","l":"Ingredient"},{"p":"com.company","l":"Machine"},{"p":"com.company","l":"Main"},{"p":"com.company","l":"Menu"}]

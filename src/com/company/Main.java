d89ac3fe1faca5b65795e65f11bd8b851a7108ed
Chotiwit <nut.chotiwit@gmail.com>
2020-11-23 14:15:54 +0700

    fix if cond

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,12 +32,14 @@ public class Main {
             }
             System.out.print("\nDone ? " + "  " + "Yes or No : ");
             isDone = scn.next().toLowerCase();
-                if ((isDone.equals("no") || !isDone.equals("yes")) && (isDone.equals("yes") || !isDone.equals("no"))) {
+                    if ((isDone.equals("no") || !isDone.equals("yes")) && (isDone.equals("yes") || !isDone.equals("no"))) {
+                        do{
+                        System.out.println("Please Try Again");
+                        System.out.print("Done ?" + "  " + "Yes or No :");
+                        isDone = scn.next().toLowerCase();
+                        }while ((isDone.equals("no") || !isDone.equals("yes")) && (isDone.equals("yes") || !isDone.equals("no")));
+                    }
 
-                    System.out.println("Please Try Again");
-                    System.out.print("Done ?" + "  " + "Yes or No :");
-                    isDone = scn.next().toLowerCase();
-                }
         } while (isDone.equals("no") );
         System.out.println("===== Thank You <3 =====");
     }
